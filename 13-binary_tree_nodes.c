a0cca5301fd85a7ddc3b0e37a05cbfb7c48ad751
KenNyagz <kennyagz681@gmail.com>
2024-01-03 22:19:45 +0300

    internal nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,11 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
-		return (1);
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 
-	left_nodes = binary_tree_nodes(tree->left);
-	right_nodes = binary_tree_nodes(tree->right);
-
-	return (left_nodes + right_nodes);
-
+	return (0);
 }
