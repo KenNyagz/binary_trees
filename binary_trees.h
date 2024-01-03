e1f77453ebab18ccc1e8bf70bfa954c81d1845a5
KenNyagz <kennyagz681@gmail.com>
2024-01-03 10:31:04 +0300

    Creating new binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -28,5 +30,11 @@ typedef struct binary_tree_s avl_t;
 
 /*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
+
+/*static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);*/
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 
 #endif
